7a9e3625a471b1ac9d4ea51bde29214d57e10de1
Amrinder <amrinder@debian.laptop>
2024-09-21 21:40:16 +0530

    Uploading file using frappe now

--- drillbit/drillbit/doctype/assignment/assignment.py
+++ drillbit/drillbit/doctype/assignment/assignment.py
@@ -5,7 +5,18 @@ import frappe
 from frappe.model.document import Document
 from assets.drillbit.drillbit_api import DrillbitAPI
 
+
+def get_absolute_path(file_name):
+    if(file_name.startswith('/files/')):
+        file_path = f'{frappe.utils.get_bench_path()}/sites/{frappe.utils.get_site_base_path()[2:]}/public{file_name}'
+    if(file_name.startswith('/private/')):
+        file_path = f'{frappe.utils.get_bench_path()}/sites/{frappe.utils.get_site_base_path()[2:]}{file_name}'
+    return file_path
+
 class Assignment(Document):
+    def on_save(self):
+        print("hi")
+
     def on_submit(self):
         # Assuming upload_assignment is a field of type 'Attach'
         uploaded_file = self.upload_assignment
@@ -16,13 +27,20 @@ class Assignment(Document):
         # Extract username and password
         username = drillbit_settings.username
         password = drillbit_settings.get_password('password')
-        current_directory = os.getcwd()
-        frappe.msgprint(current_directory)
+        # current_directory = os.getcwd()
+        # frappe.msgprint(current_directory)
         # Authenticate with Drillbit API
         base_url = "https://s1.drillbitplagiarismcheck.com"
-        # api = DrillbitAPI(base_url)
+        api = DrillbitAPI(base_url)
         # api.authenticate(username, password, frappe)
         # api.create_folder("New Folder")
+        folder_id=450824
+        file_path=get_absolute_path(uploaded_file)
+        author_name="Amrinder Singh"
+        title="Effects of Air Space in ABF"
+        document_type="Thesis"
+        api.upload_file("Amrinder Singh", "Effects of Air Space in ABF 2004", "Thesis", "amrinder2676@gmail.com", "Amrinder", "YES", "NO", "English", file_path)
 
-        frappe.msgprint(f"This is the Uploaded file: {frappe.get_site_path()}{uploaded_file}")
+
+        frappe.msgprint(f"This is the Uploaded file: {file_path}")
         frappe.msgprint(f"Username: {username}, Password: {password}")
